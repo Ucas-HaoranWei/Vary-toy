545abb29855c1619c3499b52ad40ab60fdd00e5a
WeiHaoran <50487563+Ucas-HaoranWei@users.noreply.github.com>
2024-01-22 20:57:16 +0800

    Add files via upload

---
+++ Vary-master/vary/model/__init__.py
@@ -0,0 +1,4 @@
+from .vary_opt import varyOPTModel, varyOPTForCausalLM
+# from .vary_qwen_vary import varyQwenModel, varyQwenForCausalLM, varyConfig
+from .vary_toy_qwen1_8 import varyQwenModel, varyQwenForCausalLM, varyConfig
+
